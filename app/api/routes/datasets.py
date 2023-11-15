e2a3f8cad674281373b94308d86b6dc5b85b526a
Mental <Alekov99@mail.ru>
2023-11-15 18:36:05 +0500

    Add get_datasets request

--- app/api/routes/datasets.py
+++ app/api/routes/datasets.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter, UploadFile
+import os
+from typing import Annotated
+
+from fastapi import APIRouter, UploadFile, File
 
 from ...services.dataset_processor import uploader
+from ...constants.artifacts import PATHS
 
 router = APIRouter()
 
 
 @router.post("/upload")
-async def upload_dataset(dataset: UploadFile):
-    await uploader.upload_dataset(dataset)
+async def upload_datasets(datasets: Annotated[list[UploadFile], File(description="Multiple datasets")]) -> None:
+    await uploader.upload_datasets(datasets)
+
+
+@router.get("/")
+async def get_datasets() -> list[str]:
+    return os.listdir(PATHS["DATASETS_PATH"])
--- app/services/dataset_processor/uploader.py
+++ app/services/dataset_processor/uploader.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 
 from fastapi import UploadFile
 
@@ -8,3 +9,7 @@ from ..file_manager.file import write_file
 
 def upload_dataset(file: UploadFile):
   return write_file(os.path.join(PATHS['DATASETS_PATH'], file.filename), file)
+
+
+def upload_datasets(files: list[UploadFile]):
+  return asyncio.gather(*[upload_dataset(file) for file in files])
