65b746c58f37d00dc5ba6e60b582bfb9428208dc
Mental <Alekov99@mail.ru>
2024-05-22 17:07:37 +0500

    Fix

--- app/services/experiment/run_loader/utils.py
+++ app/services/experiment/run_loader/utils.py
@@ -3,8 +3,8 @@ from .mlflow_run_loader import MLFlowRunLoader
 from .neptune_run_loader import NeptuneRunLoader
 
 
-def get_run_loader(experiment_tracker_type: ExperimentTrackerEnum, project: str, run_id: str, **kwargs):
-    if experiment_tracker_type == ExperimentTrackerEnum.MLflow:
+def get_run_loader(experiment_tracker_type: str, project: str, run_id: str, **kwargs):
+    if experiment_tracker_type == ExperimentTrackerEnum.MLflow.value:
         return MLFlowRunLoader(project=project, run_id=run_id)
-    elif experiment_tracker_type == ExperimentTrackerEnum.Neptune:
+    elif experiment_tracker_type == ExperimentTrackerEnum.Neptune.value:
         return NeptuneRunLoader(project=project, run_id=run_id, api_token=kwargs['api_token'])
